2023eda2cfb40ccf37430a5973e32b2868b1ffb9
Josh <54910913+JoshW-7@users.noreply.github.com>
2020-12-01 16:37:23 -0800

    Improve using itertools.combinations instead of triple-nested for loop

--- 2020/Day 1/part2.py
+++ 2020/Day 1/part2.py
@@ -1,13 +1,13 @@
+import itertools as it
 
 
 with open("input.txt") as file:
     entries = [int(line) for line in file.readlines()]
 
-result = 0
-for entry in entries:
-    for second_entry in entries:
-        for third_entry in entries:
-            if entry + second_entry + third_entry == 2020:
-                result = entry * second_entry * third_entry
-                
+combos = it.combinations(entries, 3)
+for first,second,third in combos:
+    if first + second + third == 2020:
+        result = first * second * third
+        break
+
 print(result)
