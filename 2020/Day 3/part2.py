2eb1e8955b8bdc7aa3aa6fda483900b1b744b268
Josh <54910913+JoshW-7@users.noreply.github.com>
2020-12-02 21:50:54 -0800

    Use modulo operator instead of if for repeating x coordinates

--- 2020/Day 3/part2.py
+++ 2020/Day 3/part2.py
@@ -11,10 +11,10 @@ for xv,yv in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]:
     count = 0
     x = 0
     y = 0
-    while y < len(levels):
-        if x >= len(levels[0]):
-            x = (x - len(levels[0]))
-        value = levels[y][x]
+    height = len(levels)
+    width = len(levels[0])
+    while y < height:
+        value = levels[y][x % width]
         if value == "#":
             count += 1
         x += xv
