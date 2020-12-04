f53f1f45d635c4cc3231e3772d476aa07220ec73
Josh <54910913+JoshW-7@users.noreply.github.com>
2020-12-04 09:38:44 -0800

    Refactor part 2

--- 2020/Day 4/part2.py
+++ 2020/Day 4/part2.py
@@ -1,113 +1,46 @@
 import re
 
 
-with open("input.txt") as file:
-    data = []
-    for line in file.readlines():
-        line = line.strip("\n")
-        data.append(line)
-
-passports = []
-lines = []
-for line in data:
-    if len(line) > 0:
-        lines.append(line)
-    else:
-        passports.append(lines)
-        lines = []
-
-data = []
-for p in passports:
-    p = " ".join([line for line in p])
-    data.append(p)
-
-
-def valid(line):
-    if "byr" not in pd:
-        return False
-    else:
-        value = int(pd["byr"])
-        if value < 1920 or value > 2002:
-            return False
-            
-    if "iyr" not in pd:
-        return False
-    else:
-        value = int(pd["iyr"])
-        if value < 2010 or value > 2020:
+def passport_valid(d):
+    for attribute in ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]:
+        if attribute not in d:
             return False
 
-    if "eyr" not in pd:
-        return False
-    else:
-        value = int(pd["eyr"])
-        if value < 2020 or value > 2030:
+    for key,value in d.items():
+        valid = {
+            "byr": lambda value: 1920 <= int(value) <= 2002,
+            "iyr": lambda value: 2010 <= int(value) <= 2020,
+            "eyr": lambda value: 2020 <= int(value) <= 2030,
+            "hgt": lambda value: 150 <= int(value[:-2]) <= 193 if value.endswith("cm") else (59 <= int(value[:-2]) <= 76 if value.endswith("in") else False),
+            "hcl": lambda value: value.startswith("#") and len(value[1:]) == 6 and bool(re.match(r"#[0-9a-f]{6}", value)),
+            "ecl": lambda value: value in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"],
+            "pid": lambda value: len(value) == 9 and value.isdigit(),
+            "cid": lambda value: True,
+        }[key](value)
+        if not valid:
             return False
-
-    if "hgt" not in pd:
-        return False
-    else:
-        num_str = pd["hgt"]
-        if num_str.endswith("cm"):
-            num = int(num_str[:num_str.find("cm")])
-            if num < 150 or num > 193:
-                return False
-        elif num_str.endswith("in"):
-            num = int(num_str[:num_str.find("in")])
-            if num < 59 or num > 76:
-                return False
-        else:
-            return False
-
-    if "hcl" not in pd:
-        return False
-    else:
-        hair = pd["hcl"]
-        if not hair.startswith("#"):
-            return False
-        else:
-            m = re.match(r"#([a-fA-F0-9]{6})", hair)
-            if m:
-                for c in m.group(1):
-                    if c not in ["a", "b", "c", "d", "e", "f", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
-                        return False
-            else:
-                return False
-
-    if "ecl" not in pd:
-        return False
-    else:
-        eye = pd["ecl"]
-        found = False
-        for color in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]:
-            if color in eye:
-                found = True
-                break
-        if not found:
-            return False
-
-    if "pid" not in pd:
-        return False
-    else:
-        pid = pd["pid"]
-        m = re.match(r"([0-9]{9})", pid)
-        if not m:
-            return False
-        if len(pid) > 9:
-            return False
-
     return True
 
-count = 0
-for line in data:
-    pairs = line.split(" ")
-    pd = {}
-    for pair in pairs:
-        key,value = pair.split(":")
-        pd[key] = value
-
-    if valid(pd):
-        count += 1
-
-print(count)
+with open("input.txt") as file:
+    lines = file.read().split("\n")
+    passports = []
+    data = ""
+    for line in lines:
+        if len(line) == 0:
+            passports.append(data.rstrip(" "))
+            data = ""
+        else:
+            data += f"{line} "
     
+    count = 0
+    for data in passports:
+        attributes = data.split(" ")
+        d = {}
+        for attribute in attributes:
+            key,value = attribute.split(":")
+            d[key] = value
+
+        if passport_valid(d):
+            count += 1
+    
+print(count)
