e9bc01d784cc6e3f7885566c4ed624e91bd38591
Josh <54910913+JoshW-7@users.noreply.github.com>
2020-12-06 11:10:44 -0800

    Remove for loop for set intersection update, as intersection_update can take an iterable

--- 2020/Day 6/part2.py
+++ 2020/Day 6/part2.py
@@ -1,15 +1,12 @@
 
 
 with open("input.txt") as file:
-    groups = file.read().split("\n\n")
-
-count = 0
-for group in groups:
-    people = [set(person) for person in group.split("\n")]
-    common = people[0]
-    if len(people) > 0:
-        for person in people:
-            common.intersection_update(person)
-    count += len(common)
+    count = 0
+    for group in file.read().split("\n\n"):
+        people = [set(person) for person in group.split("\n")]
+        common = people[0]
+        if len(people) > 0:
+            common.intersection_update(*people[1:])
+        count += len(common)
 
 print(count)
