a00825c7c2359275f3decdb93d96e732f0dafd4c
Josh <54910913+JoshW-7@users.noreply.github.com>
2020-12-05 21:40:19 -0800

    Made code smaller by reading input with double-newline delimeter and other improvements

--- 2020/Day 6/part2.py
+++ 2020/Day 6/part2.py
@@ -1,23 +1,14 @@
 
 
 with open("input.txt") as file:
-    lines = file.read().split("\n")
+    groups = file.read().split("\n\n")
 
-groups = []
-people = []
-for line in lines:
-    if line == "":
-        groups.append(people)
-        people = []
-    else:
-        person = set(line)
-        people.append(person)
-        
 count = 0
 for group in groups:
-    common = group[0]
-    if len(group) > 0:
-        for person in group[1:]:
+    people = [set(person) for person in group.split("\n")]
+    common = people[0]
+    if len(people) > 0:
+        for person in people:
             common.intersection_update(person)
     count += len(common)
 
