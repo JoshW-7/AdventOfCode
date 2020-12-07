dc85c254802b3d3786e96dac94d2cdd26b9963e3
Josh <54910913+JoshW-7@users.noreply.github.com>
2020-12-06 21:46:22 -0800

    Day 7

---
+++ 2020/Day 7/part1.py
@@ -0,0 +1,33 @@
+import re
+
+
+def contains_bag(bag_type, bag):
+    if bag_type in mapping[bag]:
+        return True
+    else:
+        for sub_bag,amount in mapping[bag].items():
+            if contains_bag(bag_type, sub_bag):
+                return True
+    return False
+
+with open("input.txt") as file:
+    lines = file.read().split("\n")
+    mapping = {}
+    for line in lines:
+        bag_type = line[0:line.find(" bags")]
+        contains = line[line.find(" contain ")+len(" contain "):]
+        bags = []
+        if not contains.startswith("no"):
+            bags = contains.split(", ")
+        mapping[bag_type] = {}
+        for text in bags:
+            amount = int(text[0])
+            other_bag = text[2:text.find(" bag")]
+            mapping[bag_type][other_bag] = amount
+
+count = 0
+for bag_type,data in mapping.items():
+    if contains_bag("shiny gold", bag_type):
+        count += 1
+
+print(count)
---
+++ 2020/Day 7/part2.py
@@ -0,0 +1,26 @@
+import re
+
+
+def count_bags(bag):
+    count = 0
+    for sub_bag,amount in mapping[bag].items():
+        count += amount
+        count += amount * count_bags(sub_bag)
+    return count
+    
+with open("input.txt") as file:
+    lines = file.read().split("\n")
+    mapping = {}
+    for line in lines:
+        bag_type = line[0:line.find(" bags")]
+        contains = line[line.find(" contain ")+len(" contain "):]
+        bags = []
+        if not contains.startswith("no"):
+            bags = contains.split(", ")
+        mapping[bag_type] = {}
+        for text in bags:
+            amount = int(text[0])
+            other_bag = text[2:text.find(" bag")]
+            mapping[bag_type][other_bag] = amount
+
+print(count_bags("shiny gold"))
