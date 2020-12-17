09fb4f00bdc846862dd409ef17e16a517d609100
Josh <54910913+JoshW-7@users.noreply.github.com>
2020-12-17 11:42:21 -0800

    N-dimensional solution because why not

--- 2020/Day 17/part2.py
+++ 2020/Day 17/part2.py
@@ -3,6 +3,7 @@ import time
 import copy
 from itertools import product
 
+dimension = 4
 
 with open("input.txt") as file:
     lines = [[c for c in line] for line in file.read().split("\n")]
@@ -10,19 +11,20 @@ with open("input.txt") as file:
     cubes = {}
     for y,line in enumerate(lines):
         for x,cube in enumerate(line):
-            cubes[(x, y, 0, 0)] = cube
+            cubes[(x, y, *(0 for i in range(dimension-2)))] = cube
 
-relative_coords = [coord for coord in product([-1, 0, 1], repeat=4) if coord != (0, 0, 0, 0)]
+relative_coords = [coord for coord in product([-1, 0, 1], repeat=dimension) if coord != (0, *(0 for i in range(dimension-1)))]
 
-def get_neighbors(x, y, z, w):
+def get_neighbors(*coords):
     global cubes
     global next_cubes
     global relative_coords
+    global dimension
 
     new_coords = set()
     active_neighbors = 0
     for coord in relative_coords:
-        rel_coord = (x+coord[0], y+coord[1], z+coord[2], w+coord[3])
+        rel_coord = (coords[0]+coord[0], *(coords[i]+coord[i] for i in range(1, dimension)))
         if rel_coord not in cubes:
             new_coords.add(rel_coord)
         if rel_coord in cubes:
@@ -30,34 +32,23 @@ def get_neighbors(x, y, z, w):
                 active_neighbors += 1
 
     return active_neighbors, new_coords
-    
-def print_layer(z, w, cubes):
-    coords = sorted([(coord[0],coord[1]) for coord in cubes if coord[2] == z and coord[3] == w])
-    size = round(math.sqrt(len(coords)))
-    layer = [[None for x in range(size)] for y in range(size)]
-    for (x,y) in coords:
-        layer[y][x] = cubes[(x, y, z, w)]
-    for row in layer:
-        print("".join(row))
-    print()
 
 cycle = 0
 while cycle < 6:
-    # print_layer(0, 0, cubes)
     next_cubes = copy.deepcopy(cubes)
-    for (x,y,z,w),cube in cubes.items():
-        active_neighbors, new_coords = get_neighbors(x, y, z, w)
+    for coords,cube in cubes.items():
+        active_neighbors, new_coords = get_neighbors(*coords)
         if cube == "#" and active_neighbors not in [2, 3]:
-            next_cubes[(x, y, z, w)] = "."
+            next_cubes[coords] = "."
         elif cube == "." and active_neighbors == 3:
-            next_cubes[(x, y, z, w)] = "#"
-        for (nx,ny,nz,nw) in new_coords:
-            next_cubes[(nx, ny, nz, nw)] = "."
-            active_neighbors, _ = get_neighbors(nx, ny, nz, nw)
-            if next_cubes[(nx, ny, nz, nw)] == "#" and active_neighbors not in [2, 3]:
-                next_cubes[(nx, ny, nz, nw)] = "."
-            elif next_cubes[(nx, ny, nz, nw)] == "." and active_neighbors == 3:
-                next_cubes[(nx, ny, nz, nw)] = "#"
+            next_cubes[coords] = "#"
+        for new_coord in new_coords:
+            next_cubes[new_coord] = "."
+            active_neighbors, _ = get_neighbors(*new_coord)
+            if next_cubes[new_coord] == "#" and active_neighbors not in [2, 3]:
+                next_cubes[new_coord] = "."
+            elif next_cubes[new_coord] == "." and active_neighbors == 3:
+                next_cubes[new_coord] = "#"
 
     cubes = next_cubes
     cycle += 1
